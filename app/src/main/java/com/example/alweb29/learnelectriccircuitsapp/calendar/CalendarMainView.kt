39bc77a28e73b59107c9ca41c93bbb387d3c7fcf
alweb29 <albertow55@gmail.com>
2023-10-16 19:16:07 +0200

    Implemented setting up time by user. In the future possible to use to send notifications at this time.

--- app/src/main/java/com/example/alweb29/learnelectriccircuitsapp/calendar/CalendarMainView.kt
+++ app/src/main/java/com/example/alweb29/learnelectriccircuitsapp/calendar/CalendarMainView.kt
@@ -1,6 +1,9 @@
 package com.example.alweb29.learnelectriccircuitsapp.calendar
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TimePicker
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.alweb29.learnelectriccircuitsapp.R
 
@@ -8,5 +11,21 @@ class CalendarMainView : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calendar_main)
+
+        val timePicker : TimePicker = findViewById(R.id.tp_calendar)
+        var hours : Int? = null 
+        var minutes : Int? = null 
+
+        timePicker.setOnTimeChangedListener{
+            view, hourOfDay, minute ->
+                hours = hourOfDay
+                minutes = minute
+        }
+        
+        val btnSubmit : Button = findViewById(R.id.btn_submit_calendar)
+        btnSubmit.setOnClickListener{
+            Toast.makeText(this, "time set to $hours hours and $minutes minutes", Toast.LENGTH_SHORT).show()
+        }
+        
     }
 }
