d5c71b70f3876ff3c9fae71bd803c14a8aae3e33
alweb29 <albertow55@gmail.com>
2023-11-08 18:04:42 +0100

    Finished work on adding FlashNotes by user-need to fix this in order to work

--- app/src/main/java/com/example/alweb29/learnelectriccircuitsapp/flashnotes/FlashNotesEditView.kt
+++ app/src/main/java/com/example/alweb29/learnelectriccircuitsapp/flashnotes/FlashNotesEditView.kt
@@ -2,6 +2,7 @@ package com.example.alweb29.learnelectriccircuitsapp.flashnotes
 
 import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
@@ -12,6 +13,7 @@ import com.example.alweb29.learnelectriccircuitsapp.MainActivity
 import com.example.alweb29.learnelectriccircuitsapp.R
 import com.example.alweb29.learnelectriccircuitsapp.dividers.DividersMainView
 import com.example.alweb29.learnelectriccircuitsapp.flashnotes.data.FlashNote
+import com.example.alweb29.learnelectriccircuitsapp.flashnotes.data.UserFlashNotesData.flashNotes
 import java.lang.StringBuilder
 
 
@@ -25,20 +27,26 @@ class FlashNotesEditView : AppCompatActivity(){
     private var etShortText : EditText? = null
     private var etLongText : EditText? = null
     private var flashNote : FlashNote? = null
+    private var currentIndex = 0
 
-    val sharedPrefsFlashNotesList = getSharedPreferences("UserFlashNotes", Context.MODE_PRIVATE)
+    private val sharedPrefsFlashNotesList: SharedPreferences by lazy {
+        getSharedPreferences("UserFlashNotes", Context.MODE_PRIVATE)
+    }
 
     private var flashNotes : ArrayList<FlashNote> = ArrayList()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_flashnotes_edit_view)
-        //TODO work on saving and making new flash notes
+
 
         btnCreate = findViewById(R.id.tv_add_flash_note)
         btnDelete = findViewById(R.id.tv_delete_flash_note)
         btnGoBackArrow = findViewById(R.id.tv_back_arrow_flash_notes)
         etShortText = findViewById(R.id.et_short_text)
         etLongText = findViewById(R.id.et_long_text)
+
+        tvPrev = findViewById(R.id.tv_prev_flash_note)
+        tvNext = findViewById(R.id.tv_next_flash_note)
 
         btnCreate!!.setOnClickListener {
             createFlashNote()
@@ -58,44 +66,90 @@ class FlashNotesEditView : AppCompatActivity(){
         tvNext!!.setOnClickListener {
             getPrevFlashNote()
         }
-        //add empty flashNote at the end
-        //flashNotes.add(FlashNote())
+
+        addDefaultFlashNote()
     }
 
     private fun getPrevFlashNote() {
-        TODO("Not yet implemented")
+        if (currentIndex <= 0){
+            currentIndex = flashNotes.size -1
+            flashNote = flashNotes[flashNotes.size - 1]
+        }else{
+            currentIndex--
+            flashNote = flashNotes[currentIndex]
+        }
+        refreshCurrentFlashNoteView()
     }
 
     private fun getNextFlashNote() {
-        TODO("Not yet implemented")
+        if (currentIndex >= flashNotes.size - 1){
+            currentIndex = 0
+            flashNote = flashNotes[0]
+        }else{
+            currentIndex++
+            flashNote = flashNotes[currentIndex]
+        }
+        refreshCurrentFlashNoteView()
     }
 
-    private fun deleteFlashNote() {
-        TODO("Not yet implemented")
-        //get index of flashnote
-        //delete flash note with this note+index - clear preferences
-        //edit flashnotes - with deleted flash note
+    private fun refreshCurrentFlashNoteView(){
+        etShortText!!.setText(flashNote!!.shortDefinition)
+        etLongText!!.setText(flashNote!!.longDefinition)
+    }
 
-        //addFlashNotesNamesToPreferences()
-        //addFlashNotesToPreferences()
+    private fun addDefaultFlashNote(){
+        flashNotes.add(FlashNote("Add header","Add text"))
+        refreshCurrentFlashNoteView()
+    }
+    private fun deleteFlashNote() {
+
+        //get index of flashnote
+        val index = flashNotes.indexOf(flashNote)
+        //if we are currently on last flash note then don't proceed because this flashNote is for adding other
+        if (index == flashNotes.size-1){
+            return
+        }
+        //delete flash note with this note+index - clear preferences
+        getNextFlashNote()
+        flashNotes.removeAt(index)
+        addFlashNotesNamesToPreferences()
+        addFlashNotesToPreferences()
+        getFlashNotesFromPreferences()
 
         Toast.makeText(this, "Deleted this Flash Note", Toast.LENGTH_SHORT).show()
         getPrevFlashNote()
     }
 
     private fun createFlashNote(){
-        TODO("Not yet implemented")
+
         //read edittext and create flash note
-        //add flash note to flashnotes
+        if (etLongText!!.text.isNotEmpty() && etShortText!!.text.isNotEmpty()){
+            val newFlashNote = FlashNote(etLongText!!.text.toString(), etShortText!!.text.toString())
+            //add flash note to flashnotes
+            flashNotes.add(flashNotes.size-1, newFlashNote)
+            flashNote = newFlashNote
 
-        //addFlashNotesNamesToPreferences()
-        //addFlashNotesToPreferences()
-        //getFlashNotesNamesFromPreferences()
+            addFlashNotesNamesToPreferences()
+            addFlashNotesToPreferences()
+            getFlashNotesFromPreferences()
 
-        Toast.makeText(this, "Added this Flash Note", Toast.LENGTH_SHORT).show()
-        getNextFlashNote()
+            Toast.makeText(this, "Added this Flash Note", Toast.LENGTH_SHORT).show()
+            getNextFlashNote()
+        }
     }
 
+    private fun deleteAllFlashNotes(){
+        for (flashNoteName in getFlashNotesNamesFromPreferences()){
+            val sharedPreferences = getSharedPreferences(flashNoteName, Context.MODE_PRIVATE)
+            val editor = sharedPreferences.edit()
+            editor.clear()
+            editor.apply()
+        }
+        flashNotes.clear()
+        addDefaultFlashNote()
+        refreshCurrentFlashNoteView()
+        addFlashNotesNamesToPreferences()
+    }
     private fun addFlashNotesNamesToPreferences(){
         val editor = sharedPrefsFlashNotesList.edit()
         editor.clear()
@@ -130,12 +184,14 @@ class FlashNotesEditView : AppCompatActivity(){
     }
 
     private fun getFlashNotesFromPreferences(){
+        flashNotes.clear()
         for (name in getFlashNotesNamesFromPreferences()){
             val sharedPrefsFlashNote = getSharedPreferences(name, Context.MODE_PRIVATE)
             val content : List<String> = sharedPrefsFlashNote.getString(name, "")!!.split(";")
             val readFlashNote =FlashNote(content[0],content[1])
             flashNotes.add(readFlashNote)
         }
+        addDefaultFlashNote()
     }
 
 }
