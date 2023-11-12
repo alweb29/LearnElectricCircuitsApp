23c2c679829957fca85a75698a1901eb137d23c3
alweb29 <albertow55@gmail.com>
2023-11-12 16:48:37 +0100

    Added intents between main view and proper classes in TheveninNortonMainView.kt

---
+++ app/src/main/java/com/example/alweb29/learnelectriccircuitsapp/theveninnorton/TheveninMainView.kt
@@ -0,0 +1,12 @@
+package com.example.alweb29.learnelectriccircuitsapp.theveninnorton
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.alweb29.learnelectriccircuitsapp.R
+
+class TheveninMainView : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_thevenin)
+    }
+}
--- app/src/main/java/com/example/alweb29/learnelectriccircuitsapp/theveninnorton/TheveninNortonMainView.kt
+++ app/src/main/java/com/example/alweb29/learnelectriccircuitsapp/theveninnorton/TheveninNortonMainView.kt
@@ -1,13 +1,35 @@
 package com.example.alweb29.learnelectriccircuitsapp.theveninnorton
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.alweb29.learnelectriccircuitsapp.MainActivity
 import com.example.alweb29.learnelectriccircuitsapp.R
+import com.example.alweb29.learnelectriccircuitsapp.dividers.DividersMainView
 
 class TheveninNortonMainView :AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_thevenin_norton_main)
 
+        val theveninBtn : TextView = findViewById(R.id.tv_thevenin_method)
+        val nortonBtn : TextView = findViewById(R.id.tv_norton_method)
+        val goBackArrow : ImageButton = findViewById(R.id.ib_go_back_arrow_dividers)
+
+        theveninBtn.setOnClickListener {
+            val intent = Intent(this, TheveninMainView::class.java)
+            startActivity(intent)
+        }
+        nortonBtn.setOnClickListener {
+            val intent = Intent(this, NortonMainView::class.java)
+            startActivity(intent)
+        }
+
+        goBackArrow.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
